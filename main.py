4e8c3ecb341fa7543387b8e182f65301066b268d
QSnake <jester36883688@gmail.com>
2023-10-26 16:21:53 +0800

    fucking som pic

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import uvicorn
+import wandb
+import os
 
 app = FastAPI()
 
+
+
+@app.get("/images/seed_map.png")
+async def read_image():
+    api = wandb.Api()
+    artifact = api.artifact("web_packet_prediction/som_visulization:latest")
+    # 从Artifact中获取图像文件
+    #image_file = artifact.get(image_name)
+    # 下载图像文件到本地
+    artifact.files(names=['seed_map.png'])[0].download()
+    with open("seed_map.png", "rb") as file:
+        image_data = file.read()
+    os.remove("seed_map.png")
+    return Response(content=image_data, media_type="image/png")
+
+
+@app.get("/images/u_map.png")
+async def read_image():
+    api = wandb.Api()
+    artifact = api.artifact("web_packet_prediction/som_visulization:latest")
+    # 从Artifact中获取图像文件
+    #image_file = artifact.get(image_name)
+    # 下载图像文件到本地
+    artifact.files(names=['u_map.png'])[0].download()
+    with open("u_map.png", "rb") as file:
+        image_data = file.read()
+    os.remove("u_map.png")
+    return Response(content=image_data, media_type="image/png")
+
 ## Example
+
+
+
 
 @app.get("/")
 def read_root():
@@ -30,9 +64,28 @@ def task_status():
     return "not yet"
 
 ## Wandb 區域
-@app.get("/wandb_config")
-def wandb_config():
-    return "not yet"
+@app.get("/wandb_config_date")
+def wandb_config_date(date):
+    return {
+        "model_version":"",
+        "node_number":"",
+        "sentence_transformer":"all-mpnet-base-v2"
+    }
+
+@app.get("/model_now_detail")
+def model_now_detail():
+
+    return "today"
+
+@app.get("/wandb_som_umap")
+def wandb_som_umap():
+
+    return "today"
+
+@app.get("/wandb_seed_umap")
+def wandb_seed_umap():
+    return "today"
+
 
 
 if __name__ == "__main__":
